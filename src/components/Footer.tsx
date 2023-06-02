a6028cc37d9ae4a38899d04b1050ce98980ba763
Fouad Abdeen <abdeen.dev@gmail.com>
2023-06-02 16:26:40 +0300

    Add Footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,50 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <>
+      <footer>
+        <div className="container">
+          <div className="footer-item">
+            <a href="#" className="footer-link">
+              About Us
+            </a>
+          </div>
+          <div className="footer-item">
+            <a href="#" className="footer-link">
+              Contact Us
+            </a>
+          </div>
+          <div className="footer-item">
+            <a href="#" className="footer-link">
+              Delivery Information
+            </a>
+          </div>
+          <div className="footer-item">
+            <a href="#" className="footer-link">
+              Refund Policy
+            </a>
+          </div>
+          <div className="footer-item">
+            <a href="#" className="footer-link">
+              Private Policy
+            </a>
+          </div>
+          <div className="footer-item">
+            <a href="#" className="footer-link">
+              Terms of Service
+            </a>
+          </div>
+          <div className="footer-item">
+            <a href="#" className="footer-link">
+              Our Brands
+            </a>
+          </div>
+        </div>
+        <p className="copyright">&copy; 2023 The Sustainable Community. All rights reserved.</p>
+      </footer>
+    </>
+  );
+}
+
+export default Footer;
