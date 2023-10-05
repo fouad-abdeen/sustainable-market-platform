6b2a220476be747b3a9af18ce8f665b91b5bc170
Layla Hammoud <laylahammoud63@gmail.com>
2023-10-05 12:37:55 +0300

    finish the product alone page

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,12 @@ import "./App.css";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home/Home";
-
+import ProductDetail from "./pages/product/ProductDetail"
 function App() {
   return (
     <>
       <Navbar />
-      <Home />
+      <ProductDetail />
       <Footer/>
       {/* <Footer /> */}
     </>
